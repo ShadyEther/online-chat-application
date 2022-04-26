38d0c20aab518265ff24c7b9308277a3125c6578
ShadyEther <89960953+ShadyEther@users.noreply.github.com>
2022-04-26 09:07:16 +0530

    added favicon

--- website/views.py
+++ website/views.py
@@ -1,11 +1,21 @@
-from flask import Blueprint
+
+from flask import Blueprint, render_template
+
+
 
 views = Blueprint("views", __name__)
 
-@views.route('/')
+@views.route('/home')
 def home():
     return "<h1>This is your home page</h1>"
 
 @views.route('/hello')
 def sayHello():
     return "<h1>Hello sritam</h1>"
+
+
+@views.route('/')
+def favicon():
+    return render_template('favicon.html')
+
+
